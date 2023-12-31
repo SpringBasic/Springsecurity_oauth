9d0b51d9fc288a8c6f5aba1c9b2390433c858c00
choidongkuen <danaver12@daum.net>
2024-01-01 06:22:52 +0900

    chore : 기본 프로젝트 세팅

---
+++ src/main/java/com/springsecurity_oauth/SpringsecurityOauthApplication.java
@@ -0,0 +1,13 @@
+package com.springsecurity_oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityOauthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityOauthApplication.class, args);
+    }
+
+}
